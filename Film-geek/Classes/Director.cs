4d507e65e14e4f55e9eeec65acfbc357fe9d5a18
Vikes2 <muczynski.pawel@gmail.com>
2018-04-27 13:49:02 +0200

    add data structure

---
+++ Film-geek/Classes/Director.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Film_geek.Classes
+{
+    public class Director : Person
+    {
+        public Director()
+        {
+
+        }
+    }
+}
